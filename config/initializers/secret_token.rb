c3d10f39f4c7e5d89e37667dd834691cc6f76615
Zudochkin Dmitry <dnd.pliz@gmail.com>
2012-03-30 10:35:57 +0400

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Unobtrusive::Application.config.secret_token = 'de5cde388d1c047f7bc0ed108bb3d49f3d64f523e1617e893b78499fe91c5cc13291b881bb7c8acc3414566d77f91db8f2cfdf2643b0723d1c754e2356702dc6'

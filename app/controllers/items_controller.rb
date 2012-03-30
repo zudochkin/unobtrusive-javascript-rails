7df97a187dad82e48e77a87149112ed7a6f6b239
Zudochkin Dmitry <dnd.pliz@gmail.com>
2012-03-30 12:11:46 +0400

    ajax crud added

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,83 +1,44 @@
 class ItemsController < ApplicationController
-  # GET /items
-  # GET /items.json
-  def index
+  before_filter do
     @items = Item.all
-
-    respond_to do |format|
-      format.html # index.html.erb
-      format.json { render json: @items }
-    end
-  end
-
-  # GET /items/1
-  # GET /items/1.json
-  def show
-    @item = Item.find(params[:id])
-
-    respond_to do |format|
-      format.html # show.html.erb
-      format.json { render json: @item }
-    end
-  end
-
-  # GET /items/new
-  # GET /items/new.json
-  def new
     @item = Item.new
-
-    respond_to do |format|
-      format.html # new.html.erb
-      format.json { render json: @item }
-    end
   end
 
-  # GET /items/1/edit
-  def edit
-    @item = Item.find(params[:id])
+  def index
   end
 
-  # POST /items
-  # POST /items.json
+  def new
+    render :action => 'index'
+  end
+
   def create
     @item = Item.new(params[:item])
-
-    respond_to do |format|
-      if @item.save
-        format.html { redirect_to @item, notice: 'Item was successfully created.' }
-        format.json { render json: @item, status: :created, location: @item }
-      else
-        format.html { render action: "new" }
-        format.json { render json: @item.errors, status: :unprocessable_entity }
-      end
+    if @item.save
+      flash[:notice] = "Successfully created item."
+      @items = Item.all
     end
   end
 
-  # PUT /items/1
-  # PUT /items/1.json
+  def edit
+    @item = Item.find(params[:id])
+    @items = Item.all
+    #render :action => 'index'
+  end
+
   def update
     @item = Item.find(params[:id])
-
-    respond_to do |format|
-      if @item.update_attributes(params[:item])
-        format.html { redirect_to @item, notice: 'Item was successfully updated.' }
-        format.json { head :no_content }
-      else
-        format.html { render action: "edit" }
-        format.json { render json: @item.errors, status: :unprocessable_entity }
-      end
+    if @item.update_attributes(params[:item])
+      flash[:notice] = "Successfully updated item."
     end
+    @items = Item.all
+    render :action => 'create'
   end
 
-  # DELETE /items/1
-  # DELETE /items/1.json
   def destroy
     @item = Item.find(params[:id])
     @item.destroy
-
-    respond_to do |format|
-      format.html { redirect_to items_url }
-      format.json { head :no_content }
-    end
+    flash[:notice] = "Successfully destroyed item."
+    #redirect_to items_url
+    @items = Item.all
   end
 end
